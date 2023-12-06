d78f19dab6a94fc05dcd9f65e3f8ad1eeec9b635
rykwsk <115878437+rykwsk@users.noreply.github.com>
2023-12-06 20:44:29 +0900

    仮

--- scripts/scene0-text.js
+++ scripts/scene0-text.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    const scene = document.querySelector('a-scene');
+    /*const scene = document.querySelector('a-scene');
 
     if (scene.hasLoaded) {
         scene.enterVR();
@@ -7,12 +7,14 @@ window.onload = function(){
         scene.addEventListener('loaded', function(){
             scene.enterVR();
         });
-    }
+    }*/
 
     const nextbtn = document.getElementById("nextbtn");
     const text = document.getElementById("text");
     let linenum = 0;
     const lines = ['His name is Momotaro because he was born from a peach.'];
+
+    
     nextbtn.addEventListener("click", function() {
         if(linenum<1){
             text.setAttribute('text', "value: " + lines[linenum] + ";");
